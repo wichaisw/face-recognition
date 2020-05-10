4e937c511f9909f534aa2a3f676a07025078cf13
KradasA4 <wichai.sw19@gmail.com>
2020-05-10 18:46:09 +0700

    add condition to check regitered email duplicatoin

--- face-recognition-api/controllers/register.js
+++ face-recognition-api/controllers/register.js
@@ -1,35 +1,40 @@
-const handleRegister = (req, res, db, bcrypt) => {
+const handleRegister = async (req, res, db, bcrypt) => {
   const { email, name, password } = req.body
   const hash = bcrypt.hashSync(password, 12);
 
-  // ใช้ trx แทน db ใน transaction ถ้าอะไรผิดพลาดก็ rollback ทั้งหมด
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email, email
-    })
-    .into('login')
-    .returning('email')  // คืน resolve promise ให้ .then(loginEmail)
-    .then(loginEmail => { 
-      return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0],
-          name: name,
-          joined: new Date()
+  const existedEmail = await db.select('*').from('users').where({email: req.body.email})
+  console.log(existedEmail[0])
+  if (existedEmail[0]) {
+    res.status(400).json('This email is already existed.')
+  } else {
+    // ใช้ trx แทน db ใน transaction ถ้าอะไรผิดพลาดก็ rollback ทั้งหมด
+    db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email, email
         })
-        .then(user => {
-          res.status(201).json(user[0]);  // there should only be one registering user at a time so we use index [0] to select object in the array instead of respond with a whole object
-        }) 
+        .into('login')
+        .returning('email')  // คืน resolve promise ให้ .then(loginEmail)
+        .then(loginEmail => { 
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.status(201).json(user[0]);  // there should only be one registering user at a time so we use index [0] to select object in the array instead of respond with a whole object
+            }) 
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      
     })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-
-  .catch(err => {
-    res.status(400).json('unable to register') // don't response with err becuz we shouldn't give user any information about our server
-  });
-
+    .catch(err => {
+      res.status(400).json('unable to register') // don't response with err becuz we shouldn't give user any information about our server
+    });
+  }
 }
 
 module.exports = {
