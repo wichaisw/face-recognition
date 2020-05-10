46dcff836a42fcfca2eb03f337c1dc421a2b7cb3
KradasA4 <wichai.sw19@gmail.com>
2020-05-10 17:00:41 +0700

    remove old code that rely on json data

--- face-recognition-api/server.js
+++ face-recognition-api/server.js
@@ -14,36 +14,11 @@ const db = knex({
   }
 });
 
-// db.select('*').from('users').then(data => {
-//   console.log(data)
-// });
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
-
-// const db = {
-//   users: [
-//     {
-//       id: '01',
-//       name: 'Josh',
-//       email: 'josh@gmail.com',
-//       password: 'cookies',
-//       entries: 0,
-//       joined: new Date()
-//     },
-//     {
-//       id: '02',
-//       name: 'Sally',
-//       email: 'sally@gmail.com',
-//       password: 'banana',
-//       entries: 0,
-//       joined: new Date()
-//     }
-//   ]
-// }
 
 app.get('/', async (req, res) => {
   const users = await db.select('*').from('users');
@@ -71,41 +46,13 @@ app.post('/signin', (req, res) => {
 
     })
     .catch(err => res.status(400).json('wrong credentials'));
-
-  // rely on old json data
-  /*
-   *if (req.body.email === db.users[0].email && 
-   *  req.body.password === db.users[0].password) {
-   *    res.status(200).json(db.users[0]);
-   *  } else {
-   *    console.log(db.users)
-   *    res.status(400).json('email or password is wrong');
-   *  }
-  */
 });
 
 /* send function sets the content type to text/Html which means that the client will now treat it as text. It then returns the response to the client. The res. json function, on the other hand, sets the content-type header to application/JSON so that the client treats the response string as a valid JSON object
 */
 
 app.post('/register', (req,res) => {
-  // old db that rely on json data
-  // db.users.push(
-  //   {
-  //     id: '05',
-  //     name: name,
-  //     email: email,
-  //     password: password,
-  //     entries: 0,
-  //     joined: new Date()
-  //   }
-  // );
-
   const { email, name, password } = req.body
-  // const hash = bcrypt.genSalt(10, function(err, salt) {
-  //   bcrypt.hash(password, salt, function(err, hash) {
-  //       // Store hash in your password DB.
-  //   });
-  // });
   const hash = bcrypt.hashSync(password, 12);
 
   // ใช้ trx แทน db ใน transaction ถ้าอะไรผิดพลาดก็ rollback ทั้งหมด
@@ -136,23 +83,9 @@ app.post('/register', (req,res) => {
     res.status(400).json('unable to register') // don't response with err becuz we shouldn't give user any information about our server
   });
 
-  // old json response
-  // res.json(db.users[db.users.length-1]);
 });
 
 app.get('/profile/:id', async (req, res) => {
-  // rely on old jason data
-  /*db.users.forEach(user => {
-   *  if(user.id === req.params.id) {
-   *    found = true;
-   *    return res.status(200).json(user);
-   *  } 
-   * }); 
-   * if (!found) {
-   *    res.status(404).send('not found');
-   * }
-  */
-  
   try {
     const selectedUser = await db.select('*').from('users').where({id: req.params.id});
     // db('users').where({id: req.params.id}).select('*');
@@ -170,30 +103,16 @@ app.get('/profile/:id', async (req, res) => {
 });
 
 app.put('/image', async (req, res) => {
-  // rely on old json data
-  /*
-   * let found = false;
-   * db.users.forEach(user => {
-   *   if(user.id === req.body.id) {
-   *     found = true;
-   *     user.entries++;
-   *     return res.json(user.entries);
-   *   }
-   * });
-   * if (!found) {
-   *   res.status(404).send('not found');
-   * }
-  */
-    try {
-      const updatedEntries = await db('users')
-        .where( 'id', '=', req.body.id )
-        .increment('entries', 1)
-        .returning('entries')
+  try {
+    const updatedEntries = await db('users')
+      .where( 'id', '=', req.body.id )
+      .increment('entries', 1)
+      .returning('entries')
 
-      res.status(201).json(updatedEntries[0]);
-    } catch(err) {
-      res.status(400).send('unable to update entries');
-    }
+    res.status(201).json(updatedEntries[0]);
+  } catch(err) {
+    res.status(400).send('unable to update entries');
+  }
     
 });
 
